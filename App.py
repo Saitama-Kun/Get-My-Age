41ce0e2422d254dbdd7a5c71b32dc19bd49db6de
Saitama <rabi3dread3@gmail.com>
2016-07-28 17:07:35 +0100

    Added Some Comments To Explain the code

--- App.py
+++ App.py
@@ -1,12 +1,11 @@
 
-import datetime
-
+import datetime # It says all i think
 def main():
-   yearnow = datetime.datetime.now().year
-   put=input("Enter Your Birthday Year : ")
-   age = yearnow-int(put)
-   print("Your Age is : {} Years Old".format(age))
-   print("")
+   yearnow = datetime.datetime.now().year #Getting Actual Year Using the date time library declared in the top of code
+   put=input("Enter Your Birthday Year : ") #The Input message 
+   age = yearnow-int(put) #Converting The input Value to Integer
+   print("Your Age is : {} Years Old".format(age))  
+   print("") 
    print("Thanks For Using My App! ^_^")
 
 
